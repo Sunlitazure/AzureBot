5f95099a3dab7966ed0f305f4411ead9d967c495
Sunlitazure <technochicken@gmail.com>
2016-05-09 22:25:40 -0600

    initial checkin

--- AI_Pet/src/test/FaceTrainer.java
+++
@@ -1,9 +0,0 @@
-package test;
-
-public class FaceTrainer {
-	public static void main(String[] args){
-		String trainingDir = "data/face_recog/trevor_hatch/";
-		//Mat testImage = imread()
-		
-	}
-}
